0b11e68b900e28cc8f8ac686506013fbe3d02f66
luis <luisfelipe.bruhmuller@gmail.com>
2026-04-07 11:20:28 -0300

    ADD - if do termo,idade,casa

--- script.js
+++ script.js
@@ -54,6 +54,33 @@ window.onload = function () {
             alert("Erro: O campo horas sozinho precisa ser um número.");
             return;
         }
-       
+        if (motivo.length < 10) {
+            alert("Erro: O motivo precisa ter pelo menos 10 letras. Escreva melhor.");
+            return;
+        }
+        if (termo == false) {
+            alert("Erro: Você deve marcar que aceita os termos.");
+            return;
+        }
+        if (idade < 18) {
+            alert("Bloqueado: Você precisa ter 18 anos ou mais para adotar.");
+            return;
+        }
+        if (moradia == "apt") {
+            let permiteAnimais = prompt("O seu apartamento permite animais? (Responda sim ou não)");
+            if (permiteAnimais != "sim") {
+                alert("Bloqueado: O seu prédio precisa permitir animais!");
+                return;
+            }
+        }
+
+        if (moradia == "casa") {
+            let quintalSeguro = prompt("O seu quintal é seguro e sem buracos? (Responda sim ou não)");
+            if (quintalSeguro != "sim") {
+                alert("Bloqueado: O quintal precisa ser seguro para o pet não fugir.");
+                return;
+            }
+        }
+
     }
 }
